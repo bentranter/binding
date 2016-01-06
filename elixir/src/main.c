41f5a8559f2909ca28cffaa73eb138d6811a598c
Ben Tranter <bwtrante@lakeheadu.ca>
2016-01-05 19:06:52 -0500

    Add basic setup for Elixir binding

--- elixir/src/main.c
+++ elixir/src/main.c
@@ -4,6 +4,13 @@
 // Elixir libs -- clang doesn't know where the hell this is
 #include "erl_nif.h"
 
+// Needs to figure out what ERL_NIF_TERM means
 static ERL_NIF_TERM hello(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
 
 }
+
+static ErlNifFuncs funcs[] = {
+	{"hello", 2, hello}
+};
+
+ERL_NIF_INIT(Elixir.Hello, funcs, &hello)
