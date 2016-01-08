553b2e10575dca72a1a273ca50a885a0e0191603
Ben Tranter <bwtrante@lakeheadu.ca>
2016-01-07 22:54:47 -0500

    Use buf as var name

--- elixir/main.c
+++ elixir/main.c
@@ -1,29 +1,34 @@
 // Regular C libs
 #include <stdio.h>
 
-// Elixir libs -- clang doesn't know where the hell this is
+// Elixir libs
 #include "erl_nif.h"
+
+#define MAXLEN 1024
 
 // Needs to figure out what ERL_NIF_TERM means
 static ERL_NIF_TERM hello(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
 
 	// We need some variables
-	char *s;
+	char buf[MAXLEN];
 	int i, num;
 
 	// Grab the arguments from Elixir
-	enif_get_string(env, argv[0], s, 1024, ERL_NIF_LATIN1);
+	enif_get_string(env, argv[0], buf, MAXLEN, ERL_NIF_LATIN1);
 	enif_get_int(env, argv[1], &num);
 
 	for (i = 0; i < num; i++) {
-		printf("Hello, %s!\n", s);
+		printf("Hello, %s!\n", buf);
 	}
 
 	// Fancy version of return 0
 	return enif_make_int(env, 0);
 }
 
+// Map Elixir functions to C functions
 static ErlNifFunc funcs[] = {
+
+	// Function name in Elixir, number of arguments, C function
 	{"hello", 2, hello}
 };
 
