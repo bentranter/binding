ced6c3260e2063f046d915176fc53feebf93310d
Ben Tranter <bwtrante@lakeheadu.ca>
2016-01-10 23:52:02 -0500

    Update Mixfile

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -1,4 +1,15 @@
-defmodule Mix.Tasks.Compile.Hello do
+defmodule Hello.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :hello,
+     version: "0.0.1",
+     elixir: "~> 1.0",
+     compilers: [:make, :elixir, :app]]
+  end
+end
+
+defmodule Mix.Tasks.Compile.Make do
   @shortdoc "Compiles C code into a shared object file"
 
   def run(_) do
@@ -6,16 +17,5 @@ defmodule Mix.Tasks.Compile.Hello do
     Mix.shell.info result
 
     :ok
-  end
-end
-
-defmodule Hello.Mixfile do
-  use Mix.Project
-
-  def project do
-    [app: :hello,
-     version: "0.0.1",
-     elixir: ">= 0.14.3 and < 2.0.0",
-     compilers: [:make, :elixir, :app]]
   end
 end
